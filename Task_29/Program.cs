6342a3e5241011c5bb30dc207bfb03d5c5baa03e
Evgenia S <Dla-evgenii@yandex.ru>
2022-10-17 20:44:05 +0600

    Задача 29 (выведение на экран 8 элеметов массива)

---
+++ Task_29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random ().Next(0,100);
+    }
+}
+
+void PrintArray(int[]arr) 
+{
+   for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    } 
+} 
+
+int []array = new int[8];
+
+FillArray(array);
+PrintArray(array);
