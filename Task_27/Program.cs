b38ca68d251c8aecba4eaaf8d5e18575597aca0e
Evgenia S <Dla-evgenii@yandex.ru>
2022-10-17 20:42:45 +0600

    Задача 27 (сумма цифр вводимого числа)

---
+++ Task_27/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int GetNumber()
+{
+    int num = 0;
+    while (num <= 1)
+    {
+        Console.WriteLine("Введите число (больше 1)!");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;     
+}
+
+int NumberDigits(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void GetSumm(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"сумма цифр равна {sum}");
+}
+
+int number = GetNumber();
+int len = NumberDigits(number);
+GetSumm(number, len);
